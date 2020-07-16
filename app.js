99ed414a0ef7d05b3a94a2c8e5950f2dbe431970
kyoolbaba <kyoolbaba@gmail.com>
2020-07-17 00:06:53 +0530

    Added tables and vertical scroll

--- app.js
+++ app.js
@@ -1,330 +1,370 @@
-const list=[
+const list = [
+	{
+		name: 'Christina Woods',
+		email: 'ChristinaWoods@company.com',
+		Status: 'Active',
+		Role: 'User',
+		LastLogin: '2 Days ago',
+		Permission: 'Invalid',
+		Others: '...'
+	},
 
-    {
-        name:"Christina Woods",
-        email:'ChristinaWoods@company.com',
-        Status:'Active',
-        Role:'User',
-        LastLogin:'2 Days ago',
-        Permission:'Invalid',
-        Others:'...'
-    },
+	{
+		name: 'Joshea Fox',
+		email: 'JoshyaFox@company.com',
+		Status: 'Inactive',
+		Role: 'Admin',
+		LastLogin: '3 Days ago',
+		Permission: 'Valid',
+		Others: '...'
+	},
 
-    {
-        name:'Joshea Fox',
-        email:'JoshyaFox@company.com',
-        Status:'Inactive',
-        Role:'Admin',
-        LastLogin:"3 Days ago",
-        Permission:'Valid',
-        Others:'...'
-    },
+	{
+		name: 'Katherine White',
+		email: 'KatherineWhite@company.com',
+		Status: 'Active',
+		Role: 'Admin',
+		LastLogin: '3 Days ago',
+		Permission: 'Invalid',
+		Others: '...'
+	},
 
-    {
-        name:'Katherine White',
-        email:'KatherineWhite@company.com',
-        Status:'Active',
-        Role:'Admin',
-        LastLogin:'3 Days ago',
-        Permission:'Invalid',
-        Others:'...'
-    },
+	{
+		name: 'Mary Carlson',
+		email: 'MaryCarlson@company.com',
+		Status: 'Inactive',
+		Role: 'Admin',
+		LastLogin: '1 Day ago',
+		Permission: 'Valid',
+		Others: '...'
+	},
 
+	{
+		name: 'Nicole  Larson',
+		email: 'Nicolelarsonn@company.com',
+		Status: 'Active',
+		Role: 'User',
+		LastLogin: '1 Day ago',
+		Permission: 'Valid',
+		Others: '...'
+	},
 
-    {
-        name:'Mary Carlson',
-        email:'MaryCarlson@company.com',
-        Status:'Inactive',
-        Role:'Admin',
-        LastLogin:'1 Day ago',
-        Permission:'Valid',
-        Others:'...'
-    },
+	{
+		name: 'Raymond Pattersonn',
+		email: 'RaymondPatter@company.com',
+		Status: 'Inactive',
+		Role: 'User',
+		LastLogin: '2 Days ago',
+		Permission: 'Valid',
+		Others: '...'
+	},
 
-    {
-        name:'Nicole  Larson',
-        email:'Nicolelarsonn@company.com',
-        Status:'Active',
-        Role:'User',
-        LastLogin:'1 Day ago',
-        Permission:'Valid',
-        Others:'...'
-    },
+	{
+		name: 'Roger Carpenter',
+		email: 'RogerCarpenter@company.com',
+		Status: 'Active',
+		Role: 'User',
+		LastLogin: '2 Days ago',
+		Permission: 'Valid',
+		Others: '...'
+	},
+	{
+		name: 'Christina Woods',
+		email: 'ChristinaWoods@company.com',
+		Status: 'Active',
+		Role: 'User',
+		LastLogin: '2 Days ago',
+		Permission: 'Invalid',
+		Others: '...'
+	},
 
-    {
-        name:'Raymond Pattersonn',
-        email:'RaymondPatter@company.com',
-        Status:'Inactive',
-        Role:'User',
-        LastLogin:'2 Days ago',
-        Permission:'Valid',
-        Others:'...'
-    },
+	{
+		name: 'Joshea Fox',
+		email: 'JoshyaFox@company.com',
+		Status: 'Inactive',
+		Role: 'Admin',
+		LastLogin: '3 Days ago',
+		Permission: 'Valid',
+		Others: '...'
+	},
 
-    {
-        name:'Roger Carpenter',
-        email:'RogerCarpenter@company.com',
-        Status:'Active',
-        Role:'User',
-        LastLogin:'2 Days ago',
-        Permission:'Valid',
-        Others:'...'
-    },    {
-        name:"Christina Woods",
-        email:'ChristinaWoods@company.com',
-        Status:'Active',
-        Role:'User',
-        LastLogin:'2 Days ago',
-        Permission:'Invalid',
-        Others:'...'
-    },
+	{
+		name: 'Katherine White',
+		email: 'KatherineWhite@company.com',
+		Status: 'Active',
+		Role: 'Admin',
+		LastLogin: '3 Days ago',
+		Permission: 'Invalid',
+		Others: '...'
+	},
 
-    {
-        name:'Joshea Fox',
-        email:'JoshyaFox@company.com',
-        Status:'Inactive',
-        Role:'Admin',
-        LastLogin:"3 Days ago",
-        Permission:'Valid',
-        Others:'...'
-    },
+	{
+		name: 'Mary Carlson',
+		email: 'MaryCarlson@company.com',
+		Status: 'Inactive',
+		Role: 'Admin',
+		LastLogin: '1 Day ago',
+		Permission: 'Valid',
+		Others: '...'
+	},
 
-    {
-        name:'Katherine White',
-        email:'KatherineWhite@company.com',
-        Status:'Active',
-        Role:'Admin',
-        LastLogin:'3 Days ago',
-        Permission:'Invalid',
-        Others:'...'
-    },
+	{
+		name: 'Nicole  Larson',
+		email: 'Nicolelarsonn@company.com',
+		Status: 'Active',
+		Role: 'User',
+		LastLogin: '1 Day ago',
+		Permission: 'Valid',
+		Others: '...'
+	},
 
+	{
+		name: 'Raymond Pattersonn',
+		email: 'RaymondPatter@company.com',
+		Status: 'Inactive',
+		Role: 'User',
+		LastLogin: '2 Days ago',
+		Permission: 'Valid',
+		Others: '...'
+	},
 
-    {
-        name:'Mary Carlson',
-        email:'MaryCarlson@company.com',
-        Status:'Inactive',
-        Role:'Admin',
-        LastLogin:'1 Day ago',
-        Permission:'Valid',
-        Others:'...'
-    },
+	{
+		name: 'Roger Carpenter',
+		email: 'RogerCarpenter@company.com',
+		Status: 'Active',
+		Role: 'User',
+		LastLogin: '2 Days ago',
+		Permission: 'Valid',
+		Others: '...'
+	},
+	{
+		name: 'Christina Woods',
+		email: 'ChristinaWoods@company.com',
+		Status: 'Active',
+		Role: 'User',
+		LastLogin: '2 Days ago',
+		Permission: 'Invalid',
+		Others: '...'
+	},
 
-    {
-        name:'Nicole  Larson',
-        email:'Nicolelarsonn@company.com',
-        Status:'Active',
-        Role:'User',
-        LastLogin:'1 Day ago',
-        Permission:'Valid',
-        Others:'...'
-    },
+	{
+		name: 'Joshea Fox',
+		email: 'JoshyaFox@company.com',
+		Status: 'Inactive',
+		Role: 'Admin',
+		LastLogin: '3 Days ago',
+		Permission: 'Valid',
+		Others: '...'
+	},
 
-    {
-        name:'Raymond Pattersonn',
-        email:'RaymondPatter@company.com',
-        Status:'Inactive',
-        Role:'User',
-        LastLogin:'2 Days ago',
-        Permission:'Valid',
-        Others:'...'
-    },
+	{
+		name: 'Katherine White',
+		email: 'KatherineWhite@company.com',
+		Status: 'Active',
+		Role: 'Admin',
+		LastLogin: '3 Days ago',
+		Permission: 'Invalid',
+		Others: '...'
+	},
 
-    {
-        name:'Roger Carpenter',
-        email:'RogerCarpenter@company.com',
-        Status:'Active',
-        Role:'User',
-        LastLogin:'2 Days ago',
-        Permission:'Valid',
-        Others:'...'
-    },
-    {
-        name:"Christina Woods",
-        email:'ChristinaWoods@company.com',
-        Status:'Active',
-        Role:'User',
-        LastLogin:'2 Days ago',
-        Permission:'Invalid',
-        Others:'...'
-    },
+	{
+		name: 'Mary Carlson',
+		email: 'MaryCarlson@company.com',
+		Status: 'Inactive',
+		Role: 'Admin',
+		LastLogin: '1 Day ago',
+		Permission: 'Valid',
+		Others: '...'
+	},
 
-    {
-        name:'Joshea Fox',
-        email:'JoshyaFox@company.com',
-        Status:'Inactive',
-        Role:'Admin',
-        LastLogin:"3 Days ago",
-        Permission:'Valid',
-        Others:'...'
-    },
+	{
+		name: 'Nicole  Larson',
+		email: 'Nicolelarsonn@company.com',
+		Status: 'Active',
+		Role: 'User',
+		LastLogin: '1 Day ago',
+		Permission: 'Valid',
+		Others: '...'
+	},
 
-    {
-        name:'Katherine White',
-        email:'KatherineWhite@company.com',
-        Status:'Active',
-        Role:'Admin',
-        LastLogin:'3 Days ago',
-        Permission:'Invalid',
-        Others:'...'
-    },
+	{
+		name: 'Raymond Pattersonn',
+		email: 'RaymondPatter@company.com',
+		Status: 'Inactive',
+		Role: 'User',
+		LastLogin: '2 Days ago',
+		Permission: 'Valid',
+		Others: '...'
+	},
 
+	{
+		name: 'Roger Carpenter',
+		email: 'RogerCarpenter@company.com',
+		Status: 'Active',
+		Role: 'User',
+		LastLogin: '2 Days ago',
+		Permission: 'Valid',
+		Others: '...'
+	},
+	{
+		name: 'Christina Woods',
+		email: 'ChristinaWoods@company.com',
+		Status: 'Active',
+		Role: 'User',
+		LastLogin: '2 Days ago',
+		Permission: 'Invalid',
+		Others: '...'
+	},
 
-    {
-        name:'Mary Carlson',
-        email:'MaryCarlson@company.com',
-        Status:'Inactive',
-        Role:'Admin',
-        LastLogin:'1 Day ago',
-        Permission:'Valid',
-        Others:'...'
-    },
+	{
+		name: 'Joshea Fox',
+		email: 'JoshyaFox@company.com',
+		Status: 'Inactive',
+		Role: 'Admin',
+		LastLogin: '3 Days ago',
+		Permission: 'Valid',
+		Others: '...'
+	},
 
-    {
-        name:'Nicole  Larson',
-        email:'Nicolelarsonn@company.com',
-        Status:'Active',
-        Role:'User',
-        LastLogin:'1 Day ago',
-        Permission:'Valid',
-        Others:'...'
-    },
+	{
+		name: 'Katherine White',
+		email: 'KatherineWhite@company.com',
+		Status: 'Active',
+		Role: 'Admin',
+		LastLogin: '3 Days ago',
+		Permission: 'Invalid',
+		Others: '...'
+	},
 
-    {
-        name:'Raymond Pattersonn',
-        email:'RaymondPatter@company.com',
-        Status:'Inactive',
-        Role:'User',
-        LastLogin:'2 Days ago',
-        Permission:'Valid',
-        Others:'...'
-    },
+	{
+		name: 'Mary Carlson',
+		email: 'MaryCarlson@company.com',
+		Status: 'Inactive',
+		Role: 'Admin',
+		LastLogin: '1 Day ago',
+		Permission: 'Valid',
+		Others: '...'
+	},
 
-    {
-        name:'Roger Carpenter',
-        email:'RogerCarpenter@company.com',
-        Status:'Active',
-        Role:'User',
-        LastLogin:'2 Days ago',
-        Permission:'Valid',
-        Others:'...'
-    },    {
-        name:"Christina Woods",
-        email:'ChristinaWoods@company.com',
-        Status:'Active',
-        Role:'User',
-        LastLogin:'2 Days ago',
-        Permission:'Invalid',
-        Others:'...'
-    },
+	{
+		name: 'Nicole  Larson',
+		email: 'Nicolelarsonn@company.com',
+		Status: 'Active',
+		Role: 'User',
+		LastLogin: '1 Day ago',
+		Permission: 'Valid',
+		Others: '...'
+	},
 
-    {
-        name:'Joshea Fox',
-        email:'JoshyaFox@company.com',
-        Status:'Inactive',
-        Role:'Admin',
-        LastLogin:"3 Days ago",
-        Permission:'Valid',
-        Others:'...'
-    },
+	{
+		name: 'Raymond Pattersonn',
+		email: 'RaymondPatter@company.com',
+		Status: 'Inactive',
+		Role: 'User',
+		LastLogin: '2 Days ago',
+		Permission: 'Valid',
+		Others: '...'
+	},
 
-    {
-        name:'Katherine White',
-        email:'KatherineWhite@company.com',
-        Status:'Active',
-        Role:'Admin',
-        LastLogin:'3 Days ago',
-        Permission:'Invalid',
-        Others:'...'
-    },
+	{
+		name: 'Roger Carpenter',
+		email: 'RogerCarpenter@company.com',
+		Status: 'Active',
+		Role: 'User',
+		LastLogin: '2 Days ago',
+		Permission: 'Valid',
+		Others: '...'
+	}
+];
 
+let Tableheader = [ 'Name', 'Email', 'Status', 'Role', 'Last login', 'Permission' ];
 
-    {
-        name:'Mary Carlson',
-        email:'MaryCarlson@company.com',
-        Status:'Inactive',
-        Role:'Admin',
-        LastLogin:'1 Day ago',
-        Permission:'Valid',
-        Others:'...'
-    },
+let flexContainer = document.createElement('div');
+flexContainer.className = 'row1';
 
-    {
-        name:'Nicole  Larson',
-        email:'Nicolelarsonn@company.com',
-        Status:'Active',
-        Role:'User',
-        LastLogin:'1 Day ago',
-        Permission:'Valid',
-        Others:'...'
-    },
+Tableheader.forEach((header) => {
+	let theader = document.createElement('div');
+	theader.className = 'row-elements';
+	theader.innerText = header;
+	flexContainer.appendChild(theader);
+});
 
-    {
-        name:'Raymond Pattersonn',
-        email:'RaymondPatter@company.com',
-        Status:'Inactive',
-        Role:'User',
-        LastLogin:'2 Days ago',
-        Permission:'Valid',
-        Others:'...'
-    },
+document.querySelector('.header').appendChild(flexContainer);
 
-    {
-        name:'Roger Carpenter',
-        email:'RogerCarpenter@company.com',
-        Status:'Active',
-        Role:'User',
-        LastLogin:'2 Days ago',
-        Permission:'Valid',
-        Others:'...'
-    }
-]
+list.forEach((user) => {
+	let row = document.createElement('div');
+	row.className = 'row';
+	Object.values(user).forEach((text) => {
+		let rowelement = document.createElement('div');
+		rowelement.innerText = text;
+		rowelement.className = 'row-elements1';
+		row.appendChild(rowelement);
+	});
+	document.querySelector('.display-no-users').appendChild(row);
+});
 
+// list.forEach(user =>{
+//     let row=document.createElement('div');
+//     row.className='row';
+//     let count=0;
+//      Object.values(user).forEach( text=>{
+//          let rowdata=document.createElement('td');
+//          let textInSideTD=document.createTextNode(text);
+//          rowdata.className="user-row-data";
+//          if(count===0){
+//              let iconForUser=document.createElement('img');
+//              iconForUser.className="icon-for-user";
+//              iconForUser.src='img/user.png';
+//              rowdata.appendChild(iconForUser);
+//              }
+//          console.log(text);
+//          if(count===2){
+//              textInSideTD.textContent="";
+//             rowdata.className='status-column';
+//             let imgdata=document.createElement('img')
+//             if(text==='Active'){
+//                 imgdata.src='img/brightActive.png'
+//                 imgdata.className='status-image';
+//                 rowdata.appendChild(imgdata);
+//             //    rowdata.style.webkitBoxShadow='3px solid rgb(0, 255, 255)';
+//             }else{
+//                 imgdata.src='img/editedInactive.png'
+//                 imgdata.className='status-image';
+//                 rowdata.appendChild(imgdata);
+//                 // rowdata.style.border='3px solid rgb(200, 151, 151) ';
+//                 // rowdata.style.color='gray';
+//             }
+//          }
+//          console.log(user+'PPrinting');
+//          count++;
+//          rowdata.appendChild(textInSideTD);
+//          row.appendChild(rowdata)
+//      } )
+//      flexContainer.appendChild(row);
+// } )
 
+console.log('Hey');
 
-
-
-
-
-
-// let Tableheader=['Name','Email','Status','Role','Last login','Permission'];
-
-// let flexContainer=document.querySelector('#flex-container');
-
-// Tableheader.forEach(
-//     header => {
-//         let theader=document.createElement('div');
-//         theader.innerText=header;
-//         flexContainer.appendChild(theader);
-//         console.log(heashow-auth-list
-   
-// )
-// let table=document.querySelector('table')
-
-console.log("Hey");
-
-let authDropdown=document.querySelector('.show-auth-list');
+let authDropdown = document.querySelector('.show-auth-list');
 console.log(authDropdown);
 
-
-let authclickbutton=authDropdown.addEventListener('onclick',function(){
-    if( authDropdown.id==='show-auth-list-block'){
-       authDropdown.id='show-auth-list-display'
-    }else{
-        authDropdown.id='show-auth-list-block'
-    }
-})
-
-
-// let authClick=authDropdown.addEventListener('click', function(){
-//     console.log("function getting called");
-//    if(document.getElementById('show-auth-list-block')===null){
-//         document.querySelector('.show-auth-list').id='show-auth-list-block';
-//         console.log("printing top one");
-//         // document.querySelector('.show-auth-list').style.backgroundColor='#363A43';
-//    }else{
-//     console.log("printing top one");
-//     document.querySelector('.show-auth-list').id='show-auth-list-display';
-//     // document.querySelector('.show-auth-list').style.backgroundColor='rgb(161, 160, 161)';
-// }
+// let authclickbutton=authDropdown.addEventListener('onclick',function(){
+//     if( authDropdown.id==='show-auth-list-block'){
+//        authDropdown.id='show-auth-list-display'
+//     }else{
+//         authDropdown.id='show-auth-list-block'
+//     }
 // })
+
+let authClick = authDropdown.addEventListener('click', function() {
+	console.log('function getting called');
+	if (document.getElementById('show-auth-list-block') === null) {
+		document.querySelector('.show-auth-list').id = 'show-auth-list-block';
+		console.log('printing top one');
+		// document.querySelector('.show-auth-list').style.backgroundColor='#363A43';
+	} else {
+		console.log('printing top one');
+		document.querySelector('.show-auth-list').id = 'show-auth-list-display';
+		// document.querySelector('.show-auth-list').style.backgroundColor='rgb(161, 160, 161)';
+	}
+});
